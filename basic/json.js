6a2657d35291ffead219fd55080c9ed9f26c3df9
전찬혁 <hyukee0205@gmail.com>
2023-01-22 00:37:52 +0900

    0122

---
+++ basic/json.js
@@ -0,0 +1,48 @@
+// JSON
+
+// 1. Object 
+// stringfiy(obj)
+let json = JSON.stringify(true);
+console.log(json);
+
+json = JSON.stringify(['apple', 'banana']);
+console.log(json);
+
+const rabbit = {
+  name: 'tori',
+  color: 'white',
+  size: 'null',
+  birthDate: new Date(),
+  jump: () => {
+    console.log(`${this.name} can jump!`);
+  }
+};
+
+json = JSON.stringify(rabbit);
+console.log(json);
+
+json = JSON.stringify(rabbit, ["name", "color", "size"]);
+console.log(json);
+
+json = JSON.stringify(rabbit, (key, value) => {
+  console.log(`key: ${key}, value: ${value}`);
+  return key === 'name' ? 'hyuk' : value;
+});
+console.log(json);
+
+// 2. JSON 
+// parse(json) -> json을 오브젝트
+console.clear();
+json = JSON.stringify(rabbit);
+const obj = JSON.parse(json, (key, value) => {
+  console.log(`key: ${key}, value: ${value}`);
+  return key === 'birthDate' ? new Date(value) : value;
+});
+console.log(obj);
+rabbit.jump();
+// obj.jump();
+
+console.log(rabbit.birthDate.getDate());
+console.log(obj.birthDate.getDate());
+
+
