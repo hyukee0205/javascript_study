b0805d197cf164344af85e4f34de220c80a5c8c0
전찬혁 <hyukee0205@gmail.com>
2023-02-12 23:43:50 +0900

    js복습

---
+++ study/2.naming.js
@@ -0,0 +1,43 @@
+// 주석을 달면 됨
+/** 
+ * 여러개의 주석
+ */
+
+/** 변수 규칙 !
+ * 라틴문자(0-9, a-z, A-Z,), _
+ * 추천: caelCase (likeThis)
+ * 한국어 X
+ * 예약어 X
+ * 숫자로 시작 X
+ * 특수문자 X(_, $ 두가지는 예외)
+*/
+
+// 나쁜예제 
+let num = 20;
+
+// 좋은예제
+let myAge = 20;
+
+// 나쁜예제
+let audio1;
+let audio2;
+
+// 좋은예제
+let backgroundAudio;
+let windAudio;
+
+// 꿀팁!
+let audioBackground;
+let audioWind;
+
+
+
+
+
+
+
+
+
+
+
+
